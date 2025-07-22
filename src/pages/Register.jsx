378612e8128530ee122dbc4d4799e92f327234cd
Delimie <aloilmism@gmail.com>
2025-07-22 12:56:03 +0700

    Login and register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,30 +9,22 @@ function Register() {
     formState: { errors },
   } = useForm({
     // resolver: yupResolver(registerSchema),
-    mode: "onTouched"
+    mode: "onTouched",
   });
 
   return (
-    <div
-      className="min-h-screen flex items-center justify-center bg-cover bg-center"
-      style={{
-        backgroundImage: `url('https://i.pinimg.com/1200x/d4/97/50/d49750f776171052bc004c53c36aa90d.jpg')`,
-      }}
-    >
-      <div className="w-[370px] mx-auto">
-        <div className="mb-6 text-center">
-          <span>LOGO พ่กเราอะ</span>
-          <h1 className="text-4xl font-bold text-[#18867e] tracking-wide mb-1">
-            Create new account
-          </h1>
-        </div>
-
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center bg-[#F2EBBF] ">
+      <div className="w-[450px] mx-auto">
         <form
           onSubmit={handleSubmit()}
-          className="bg-white rounded-3xl shadow-xl px-8 py-8 flex flex-col gap-3 border border-[#bee8b0]"
+          className="bg-white rounded-3xl  shadow-xl px-16 py-8 flex flex-col gap-2"
         >
+          <h1 className="text-3xl font-bold tracking-wide mb-4 text-center">
+            Create Account
+          </h1>
+
           <FormInput
-            label="Username"
+            label="USERNAME"
             name="username"
             placeholder="Enter your username"
             register={register}
@@ -40,16 +32,23 @@ function Register() {
           />
 
           <FormInput
-            label="Email"
+            label="EMAIL"
             name="email"
             type="email"
             placeholder="Enter your email"
             register={register}
             error={errors.email?.message}
           />
+          <FormInput
+            label="PHONE"
+            name="phone"
+            placeholder="Enter your phone"
+            register={register}
+            error={errors.phone?.message}
+          />
 
           <FormInput
-            label="Password"
+            label="PASSWORD"
             name="password"
             type="password"
             placeholder="Enter your password"
@@ -58,24 +57,27 @@ function Register() {
           />
 
           <FormInput
-            label="Confirm Password"
+            label="CONFIRM PASSWORD"
             name="confirmPassword"
             type="password"
             placeholder="Confirm your password"
             register={register}
             error={errors.confirmPassword?.message}
           />
-        </form>
 
-        <div className="mt-3 text-center text-[#258178] text-sm">
-          มีบัญชีอยู่แล้ว?
-          <Link
-            to="/login"
-            className="text-[#8ca317] ml-2 font-semibold hover:text-[#2dc1b7] underline"
-          >
-            เข้าสู่ระบบ
-          </Link>
-        </div>
+          <button className="bg-[#8CBEB2] px-2 py-2 mt-4 rounded-lg font-semibold cursor-pointer hover:bg-[#5ea192] hover:scale-102 duration-120">
+            Create Account
+          </button>
+          <div className="mt-3 text-center text-[#258178] text-sm">
+            มีบัญชีอยู่แล้ว?
+            <Link
+              to="/login"
+              className="text-[#8ca317] ml-2 font-semibold hover:text-[#2dc1b7] underline"
+            >
+              เข้าสู่ระบบ
+            </Link>
+          </div>
+        </form>
       </div>
     </div>
   );
