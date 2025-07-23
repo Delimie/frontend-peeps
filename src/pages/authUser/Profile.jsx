442b5de07ff151221eee8976d027b41e4b3bed9d
auuvcp <vitchpong.v@gmail.com>
2025-07-23 16:05:16 +0700

    NewRegister,StartProfile

--- src/pages/authUser/Profile.jsx
+++ src/pages/authUser/Profile.jsx
@@ -3,9 +3,8 @@ import Avatar from "../../components/avatar";
 
 function Profile() {
   return(
-    <div className="flex flex-col items-center min-h-screen">
-      <div className="flex justify-center w-100 h-100 bg-[FFFCFC] border border-white">
-
+    <div className="flex flex-col items-center h-screen">
+      <div className="flex justify-center mt-30 w-100 h-100 bg-[FFFCFC] border">
       </div>
     </div>
   )
