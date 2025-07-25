a75da525be20dc4d21f6de21b8d7246d88d79b12
Foxfce <fenixz-1@hotmail.com>
2025-07-25 17:42:38 +0700

    add emit useEffect for when user typing and mockup textarea for chatbox (remove when front end implement thing)

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -4,6 +4,7 @@ import useAuthStore from "../stores/authStore";
 const URL = "http://localhost:8000/"
 
 export const socket = io(URL, {
+    autoConnect : false,
     auth: {
         token: useAuthStore.getState().token,
     }
